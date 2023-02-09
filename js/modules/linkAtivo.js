95233ee53d93262b8da92964d56b54cc981ce3db
Pablo Henrique <pablohdsouza.dev@gmail.com>
2023-02-09 15:48:24 -0300

    criacao de links ativos com scroll

--- js/modules/linkAtivo.js
+++ js/modules/linkAtivo.js
@@ -1,3 +1,21 @@
-export default function linkAtivo(){
-    
+import debounce from './debounce.js';
+
+export default function linkAtivo() {
+  const sections = document.querySelectorAll('.section-link');
+  const linksMenu = document.querySelectorAll('.header-menu a');
+
+  function ativarLink(index) {
+    linksMenu.forEach((link) => link.classList.remove('ativo'));
+    linksMenu[index].classList.add('ativo');
+  }
+
+  function checkScroll() {
+    sections.forEach((section, index) => {
+      if (section.getBoundingClientRect().top <= 150) ativarLink(index);
+    });
+  }
+
+  if (sections) {
+    window.addEventListener('scroll', debounce(checkScroll, 100));
+  }
 }
