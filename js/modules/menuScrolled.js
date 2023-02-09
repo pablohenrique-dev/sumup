fd87f0764a1d5dab780a2614a485dbdd553534ca
Pablo Henrique <pablohdsouza.dev@gmail.com>
2023-02-09 14:51:14 -0300

    correcao bug menu-scrolled

--- js/modules/menuScrolled.js
+++ js/modules/menuScrolled.js
@@ -1,15 +1,17 @@
+import debounce from './debounce.js';
+
 export default function menuScrolled() {
-  const header = document.querySelector('header');
+  const heroSection = document.querySelector('#hero');
   const logo = document.querySelector('header #logo');
 
   function ativarMenuScrolled() {
-    if (window.pageYOffset > 24) {
-      header.classList.add('scrolled');
+    if (window.pageYOffset > 64) {
+      heroSection.classList.add('scrolled');
       logo.setAttribute('src', './img/Logo-azul.svg');
     } else {
-      header.classList.remove('scrolled');
+      heroSection.classList.remove('scrolled');
       logo.setAttribute('src', './img/Logo.svg');
     }
   }
-  window.addEventListener('scroll', ativarMenuScrolled);
+  window.addEventListener('scroll', debounce(ativarMenuScrolled, 10));
 }
--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import menuMobile from './modules/menuMobile.js';
 import faq from './modules/faq.js';
 import menuScrolled from './modules/menuScrolled.js';
+import linkAtivo from './modules/linkAtivo.js';
 
 menuMobile();
 faq();
 menuScrolled();
+linkAtivo();
